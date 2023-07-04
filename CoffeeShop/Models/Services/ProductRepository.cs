18253ed581153c607e78a89aa64ba8e5654698ea
Raj <hinguraj77777@gmail.com>
2023-07-04 16:07:33 -0400

    Project created
    Added models, views and controllers

---
+++ CoffeeShop/Models/Services/ProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using CoffeeShop.Models.Interfaces;
+
+namespace CoffeeShop.Models.Services
+{
+    public class ProductRepository : IProductRepository
+    {
+        private List<Product> ProductsList = new List<Product>()
+        {
+            new Product{Id =1, Name = "Americano", Price = 25 , Detail = "A freshly pulled shots of espresso with hot water to achieve the size of a standard cup of brewed coffee.", ImageUrl="../images/Americano.png" },
+            new Product{Id =2, Name = "Mocha", Price = 30 , Detail = "A freshly pulled shots of espresso with hot water to achieve the size of a standard cup of brewed coffee.", ImageUrl= "../images/Americano.png" },
+            new Product{Id =3, Name = "Cafe au Lait", Price = 35 , Detail = "A freshly pulled shots of espresso with hot water to achieve the size of a standard cup of brewed coffee.", ImageUrl="../images/Americano.png" },
+
+        };
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return ProductsList;
+        }
+
+        public Product GetProductDetail(int id)
+        {
+            return ProductsList.FirstOrDefault(p => p.Id == id);
+        }
+
+        public IEnumerable<Product> GetTrendingProducts()
+        {
+            return ProductsList.Where(p => p.IsTrendingProduct);
+        }
+    }
+}
